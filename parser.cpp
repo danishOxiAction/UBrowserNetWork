b1097446e7b80e7656030e155c6b671d12ba9a0f
Kiops <sir.kiops@yandex.ru>
2016-11-24 21:49:15 +0300

    Отладка.

--- parser.cpp
+++ parser.cpp
@@ -1,44 +1,14 @@
 #include "parser.h"
-//#include <QDebug>
-//enum Tag_type {TEXT             = 0,
-//               START_TAG        = 1,
-//               END_TAG          = 2,
-//               INDEPENDENT_TAG  = 3};
-
-//class Tree
-//{
-//public:
-//    Tree(){}
-//    void push(Tag_type token_type, QString token_name, QString token_attributes){
-//        switch (token_type) {
-//        case TEXT:
-//            qDebug()<< "Type: " << "TEXT";
-//            break;
-//        case START_TAG:
-//            qDebug()<< "Type: " << "START_TAG";
-//            break;
-//        case END_TAG:
-//            qDebug()<< "Type: " << "END_TAG";
-//            break;
-//        case INDEPENDENT_TAG:
-//            qDebug()<< "Type: " << "INDEPENDENT_TAG";
-//            break;
-//        }
-//        qDebug() <<"Tag: " << token_name << ", attributes: " << token_attributes << endl;
-//    }
-//};
 
 Parser::Parser(QString raw_html)
 {
-//    Tree tree; //MFD
-
     html = raw_html;
     int pointer = 0;
 
-    QRegExp reg_text("([^><]*)");
-    QRegExp reg_scope("</?([^></]+)/?>");
-    QRegExp reg_tag("([\\S]+)");
-    QRegExp reg_attributes("\\s(.*)");
+    QRegExp reg_text("(?=\\S)([^><]*)");
+    QRegExp reg_scope("<([^><]+)/?>");
+    QRegExp reg_tag("([^\\s/]+)");
+    QRegExp reg_attributes("\\s(.*)(?!/)");
     int limit = html.size();
     while((pointer = reg_scope.indexIn( html, pointer )) != -1 && pointer < limit)
     {
